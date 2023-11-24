7eac9432cfc051bf1d974c2be0e3c70e7b4b6486
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-24 14:19:17 +0100

    inserito dati dentro i 6 oggetti creati

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 const teamMembers = [
     {
-        'name' : '',
-        'role' : '',
-        'profilePicture' : ''
+        'name' : 'Wayne Barnett',
+        'role' : 'Founder & CEO',
+        'profilePicture' : 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        'name' : '',
-        'role' : '',
-        'profilePicture' : ''
+        'name' : 'Angela Caroll',
+        'role' : 'Chief Editor',
+        'profilePicture' : 'angela-caroll-chief-editor.jpg'
     },
     {
-        'name' : '',
-        'role' : '',
-        'profilePicture' : ''
+        'name' : 'Walter Gordon',
+        'role' : 'Office Manager',
+        'profilePicture' : 'walter-gordon-office-manager.jpg'
     },
     {
-        'name' : '',
-        'role' : '',
-        'profilePicture' : ''
+        'name' : 'Angela Lopez',
+        'role' : 'Social Media Manager',
+        'profilePicture' : 'angela-lopez-social-media-manager.jpg'
     },
     {
-        'name' : '',
-        'role' : '',
-        'profilePicture' : ''
+        'name' : 'Scott Estrada',
+        'role' : 'Developer',
+        'profilePicture' : 'scott-estrada-developer.jpg'
     },
     {
-        'name' : '',
-        'role' : '',
-        'profilePicture' : ''
+        'name' : 'Barbara Ramos',
+        'role' : 'Graphic Designer',
+        'profilePicture' : 'barbara-ramos-graphic-designer.jpg '
     }
 ]
 
